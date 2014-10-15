3576b6cd82ac0547f8c7e42d62062c5c36e6ee54
Alexander Belov <git@belov.by>
2014-10-15 16:25:51 +0300

    Add logger and correct app closing

--- 1.py
+++ 1.py
@@ -1,6 +1,12 @@
 from serial import *
 from threading import Thread
 import sys
+import os
+import logging
+
+# socat -d -d pty,raw,echo=1 pty,raw,echo=1
+
+logging.basicConfig(filename='info.log',level=logging.INFO)
 
 if len(sys.argv) < 1:
   quit()
@@ -10,16 +16,19 @@ if len(sys.argv) == 3:
 else:
   baud = 9600
 
+logger = logging.getLogger("Port " + pts)
+
 class Receiver(Thread):
-    def __init__(self, serialPort):
+    def __init__(self, serialPort, pts):
         Thread.__init__(self)
         self.serialPort = serialPort
     def run(self):
         text = ""
-        while (text != "exit\n"):
+        while(True):
             text = serialPort.readline()
-            print ("\n machine1: " + text)
-        self.serialPort.close()
+            if text:
+                print ("message: " + text.rstrip('\n'))
+                logger.info("Receive message {}".format(text.rstrip('\n')))
 
 class Sender(Thread):
     def __init__(self, serialPort):
@@ -27,14 +36,22 @@ class Sender(Thread):
         self.serialPort = serialPort
     def run(self):
         text = ""
-        while(text != "exit\n"):
-            text = raw_input("Type your message>>") + "\n"
+        while(True):
+            text = raw_input("") + "\n"
+            if text == "exit\n":
+                self.serialPort.close()
+                logger.info("Exit {}".format(self.serialPort.portstr))
+                os._exit(0)
             self.serialPort.write(text)
-        self.serialPort.close()
+            logger.info("Send message {}".format(text.rstrip('\n')))
 
 serialPort = Serial("/dev/pts/" + pts, baudrate=baud)
 
+logger.info("Run port {} with speed {}".format(pts, baud))
+
 send = Sender(serialPort)
-receive = Receiver(serialPort)
+receive = Receiver(serialPort, pts)
 send.start()
 receive.start()
+
+# Use serialPort.setBaudrate("115200")
