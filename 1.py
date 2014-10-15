58353bf8cb2e319c66d54d9fcb4cfa2c89809e28
Alexander Belov <git@belov.by>
2014-10-15 17:34:18 +0300

    Fix identation

--- 1.py
+++ 1.py
@@ -19,31 +19,31 @@ else:
 logger = logging.getLogger("Port " + pts)
 
 class Receiver(Thread):
-    def __init__(self, serialPort, pts):
-        Thread.__init__(self)
-        self.serialPort = serialPort
-    def run(self):
-        text = ""
-        while(True):
-            text = serialPort.readline()
-            if text:
-                print ("message: " + text.rstrip('\n'))
-                logger.info("Receive message {}".format(text.rstrip('\n')))
+  def __init__(self, serialPort, pts):
+    Thread.__init__(self)
+    self.serialPort = serialPort
+  def run(self):
+    text = ""
+    while(True):
+      text = serialPort.readline()
+      if text:
+        print ("message: " + text.rstrip('\n'))
+        logger.info("Receive message {}".format(text.rstrip('\n')))
 
 class Sender(Thread):
-    def __init__(self, serialPort):
-        Thread.__init__(self)
-        self.serialPort = serialPort
-    def run(self):
-        text = ""
-        while(True):
-            text = raw_input("") + "\n"
-            if text == "exit\n":
-                self.serialPort.close()
-                logger.info("Exit {}".format(self.serialPort.portstr))
-                os._exit(0)
-            self.serialPort.write(text)
-            logger.info("Send message {}".format(text.rstrip('\n')))
+  def __init__(self, serialPort):
+    Thread.__init__(self)
+    self.serialPort = serialPort
+  def run(self):
+    text = ""
+    while(True):
+      text = raw_input("") + "\n"
+      if text == "exit\n":
+        self.serialPort.close()
+        logger.info("Exit {}".format(self.serialPort.portstr))
+        os._exit(0)
+      self.serialPort.write(text)
+      logger.info("Send message {}".format(text.rstrip('\n')))
 
 serialPort = Serial("/dev/pts/" + pts, baudrate=baud)
 
